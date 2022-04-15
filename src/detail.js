19125bfda12dadbf49057c5be1de47fc595973db
dmarcosc <carcharoth92@gmail.com>
2022-04-15 03:32:46 +0200

    deploy3

--- src/detail.js
+++ src/detail.js
@@ -10,14 +10,14 @@ document.querySelector('.detail')
     `
     <a class="link" href="detail.html?${nextId}">Next volume ></a>
     <div class="volume">
-        <img class="volume-img" src="${detail.images.jpg.image_url}" alt="Card image cap">
+        <img class="volume-img" src="${detail?.images.jpg.image_url}" alt="Card image cap">
         <div class="volume-body">
-            <h1 class="volume-title">${detail.title}</h1>
-            <h2 class="volume-title">${detail.title_japanese || ''}</h2>
-            <p class="volume-text">${detail.synopsis}</p>
-            <p>Year: ${detail.year || 'unknown'}</p>
+            <h1 class="volume-title">${detail?.title}</h1>
+            <h2 class="volume-title">${detail?.title_japanese || ''}</h2>
+            <p class="volume-text">${detail?.synopsis}</p>
+            <p>Year: ${detail?.year || 'unknown'}</p>
         </div>
     </div>
     <p>Trailer</p>
-    <iframe width="320" height="285"src="${detail.trailer.embed_url || 'https://www.youtube.com/embed/ZXjaTICqRf8'}"></iframe>
-    <ul class="volume-genres">Genres: ${detail.genres.map(x => '<li>'+ x.name + '</li>').join('') || 'unknown'}</ul>`;
+    <iframe width="320" height="285"src="${detail?.trailer.embed_url || 'https://www.youtube.com/embed/ZXjaTICqRf8'}"></iframe>
+    <ul class="volume-genres">Genres: ${detail?.genres.map(x => '<li>'+ x.name + '</li>').join('') || 'unknown'}</ul>`;
