7830b16399382b8032354e93950346559811607b
dmarcosc <carcharoth92@gmail.com>
2022-04-15 03:38:03 +0200

    axios

--- src/app.js
+++ src/app.js
@@ -8,25 +8,24 @@ async function makeRequest() {
     }
 
     let res = await axios(config)
-
-    console.log(res.status);
+    if (res.status == 200) {
+        const list = res.data.data
+        document.querySelector('.work')
+        .innerHTML = list.reduce((a, fw) => a +=
+        `<a class="card" href="detail.html?${fw.mal_id}">
+                <img class="card-img-top" src="${fw.images.jpg.image_url}" alt="Card image cap">
+                <div class="card-body">
+                    <h1 class="card-title">${fw.title}</h1>
+                    <h2 class="card-title">${fw.title_japanese || ''}</h2>
+                    <p class="card-text">${fw.synopsis}</p>...
+                </div>
+                <div class="card-body">
+                    <p href="#" class="card-link">More</p>
+                </div>
+            </a>`, ''
+        );
+    }
 }
 
 makeRequest();
-
-const list = require('./episodes.json')
-document.querySelector('.work')
-  .innerHTML = list.reduce((a, fw) => a +=
-  `<a class="card" href="detail.html?${fw.mal_id}">
-        <img class="card-img-top" src="${fw.images.jpg.image_url}" alt="Card image cap">
-        <div class="card-body">
-            <h1 class="card-title">${fw.title}</h1>
-            <h2 class="card-title">${fw.title_japanese || ''}</h2>
-            <p class="card-text">${fw.synopsis}</p>...
-        </div>
-        <div class="card-body">
-            <p href="#" class="card-link">More</p>
-        </div>
-    </a>`, ''
-);
   
