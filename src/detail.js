11c9f4e46d1662246d4aa55148703b97ca103b74
dmarcosc <carcharoth92@gmail.com>
2022-04-15 03:06:54 +0200

    pruebas5

--- src/detail.js
+++ src/detail.js
@@ -2,9 +2,9 @@
 const list = require('./episodes.json')
 const id = window.location.search.substr(1)
 console.log(id,window.location.search)
-const detail = list[0]
-// const nextIndex = list.findIndex((x) => x.mal_id === +id) + 1
-const nextId = 3334
+const detail = list.filter((x) => x.mal_id === +id)?.[0]
+const nextIndex = list.findIndex((x) => x.mal_id === +id) + 1
+const nextId = list[nextIndex]?.mal_id || list[0].mal_id
 document.querySelector('.detail')
   .innerHTML =
     `
